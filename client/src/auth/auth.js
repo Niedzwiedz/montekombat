44115e0b8335c530f312f256e65b6edbb79cda42
Niedzwiedz <m93.autumn@gmail.com>
2016-10-18 17:47:37 +0200

    Additional files for login and logout

---
+++ client/src/auth/auth.js
@@ -0,0 +1,35 @@
+// import {router} from './../main.js'
+/* global localStorage */
+import axios from 'axios'
+
+const LOGIN_PATH = 'http://localhost:3000/user_token'
+// const SIGNUP_PATH = 'http://localhost:3000/users'
+
+export default {
+  user: {
+    authenticated: false
+  },
+
+  login (email, password) {
+    axios.post(LOGIN_PATH, {auth: {email: email, password: password}}).then(response => {
+      localStorage.setItem('id_token', response.data)
+      this.user.authenticated = true
+      console.log(this.user.authenticated)
+    })
+  },
+
+  logout () {
+    localStorage.removeItem('id_token')
+    this.user.authenticated = false
+    console.log(this.user.authenticated)
+  },
+  checkAuth () {
+    var jwt = localStorage.getItem('id_token')
+    if (jwt) { this.user.authenticated = true } else { this.user.authenticated = false }
+  },
+  getAuthHeader () {
+    return {
+      'Authorization': 'Bearer ' + localStorage.getItem('id_token')
+    }
+  }
+}
